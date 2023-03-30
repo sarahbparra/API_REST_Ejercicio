9808d1260d3fcb3d11b60bf68097b12c1bc96112
sarah <sxrahhp@gmail.com>
2023-03-30 14:06:11 +0200

    Estado inicial

---
+++ src/main/java/com/example/ApiRestEjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestEjercicioApplication.class, args);
+	}
+
+}
